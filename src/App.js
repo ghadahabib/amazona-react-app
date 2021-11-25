aee1d4609bf10bbd31312d4709a3dc2ce0f44ab8
ghadahabib <ghada.habib94@gmail.com>
2021-11-25 20:20:30 +0200

    first launch of amazon app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+import React, { Component } from 'react';
+import data from'./data';
+class App extends React.Component {
+render() {
+    return (
+      <div className="grid-wrapper">
+      <header className="row header">
+          <div className="logo">
+              <a className = 'brand' href="/">Amazona</a>
+          </div>
+          <div>
+              <a href="/cart">Cart</a>
+              <a href="/signin">Sign In</a>
+          </div>
       </header>
-    </div>
-  );
+      {/* ENd header */}
+      <main>
+          <div className="row center">
+            {
+              data.products.map((product) => (
+                <div key={product._id} className="card">
+                  <a href = {`/product/${product._id}`} className="product-image">
+                      <img className ='medium' src={product.image} alt="product" />
+                  </a>
+                  <div className="card-body">
+                      <a href={`/product/${product._id}`}>
+                          <h2>{product.name}</h2>
+                      </a>
+                      <div className="rating">
+                          <span><i className="fa fa-star"></i></span>
+                          <span><i className="fa fa-star"></i></span>
+                          <span><i className="fa fa-star"></i></span>
+                          <span><i className="fa fa-star"></i></span>
+                          <span><i className="fa fa-star"></i></span>
+                      </div>
+                      <div className="price">
+                         $ {product.price}
+                      </div>
+                  </div>
+              </div>
+              ))
+            }
+          </div>
+          {/* End row */}
+      </main>
+      {/* End main */}
+      <footer className="row center">
+          <p>All Righs Reserved</p>
+      </footer>
+      {/* End footer */}
+  </div>
+      
+    )
+  }
 }
-
 export default App;
---
+++ src/data.js
@@ -0,0 +1,115 @@
+const data = {
+    products : [
+        {
+            _id : 1,
+            name : 'full women jacket',
+            category : 'clothes',
+            image : 'images/p1.jpg',
+            price : 120,
+            brand : 'nike',
+            rating : 4,
+            numReviews : 12,
+            description : 'heigh quality product'
+        },
+        {
+            _id : 2,
+            name : 'summer blue shirt',
+            category : 'clothes',
+            image : 'images/p2.jpg',
+            price : 180,
+            brand : 'adidas',
+            rating : 5,
+            numReviews : 8,
+            description : 'heigh quality product'
+        },
+        {
+            _id : 3,
+            name : 'winter slim dress',
+            category : 'clothes',
+            image : 'images/p3.jpg',
+            price : 260,
+            brand : 'lacosta',
+            rating : 4.5,
+            numReviews : 18,
+            description : 'heigh quality product'
+        },
+        {
+            _id : 4,
+            name : 'summer skirts with t-shirt ',
+            category : 'clothes',
+            image : 'images/p6.jpg',
+            price : 100,
+            brand : 'adidas',
+            rating : 4.5,
+            numReviews : 19,
+            description : 'heigh quality product'
+        },
+        {
+            _id : 5,
+            name : 'nike slim dress',
+            category : 'clothes',
+            image : 'images/p10.jpg',
+            price : 160,
+            brand : 'nike',
+            rating : 4.5,
+            numReviews : 10,
+            description : 'heigh quality product'
+        },
+        {
+            _id : 6,
+            name : 'winter full coat',
+            category : 'clothes',
+            image : 'images/p11.jpg',
+            price : 600,
+            brand : 'nike',
+            rating : 5,
+            numReviews : 10,
+            description : 'heigh quality product'
+        },
+        {
+            _id : 7,
+            name : 'shiny summer wear',
+            category : 'clothes',
+            image : 'images/p7.jpg',
+            price : 330,
+            brand : 'nike',
+            rating : 3,
+            numReviews : 18,
+            description : 'heigh quality product'
+        },
+        {
+            _id : 8,
+            name : 'awsome women wear',
+            category : 'clothes',
+            image : 'images/p8.jpg',
+            price : 350,
+            brand : 'nike',
+            rating : 4.8,
+            numReviews : 15,
+            description : 'heigh quality product'
+        },
+        {
+            _id : 9,
+            name : 'winter full dress',
+            category : 'clothes',
+            image : 'images/p9.jpg',
+            price : 280,
+            brand : 'nike',
+            rating : 3.5,
+            numReviews : 9,
+            description : 'heigh quality product'
+        },
+        {
+            _id : 10,
+            name : 'full sport wear',
+            category : 'clothes',
+            image : 'images/p12.jpg',
+            price : 300,
+            brand : 'nike',
+            rating : 5,
+            numReviews : 12,
+            description : 'heigh quality product'
+        }
+    ],   
+};
+export default data;
